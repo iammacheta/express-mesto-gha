c5f6dfbdc0c13fd993db2ca100b5c2781f953133
Maxim Ichetovkin <ichetovkin.maxim@gmail.com>
2022-11-11 07:47:45 +0800

    feat: создать схемы и модели, контроллеры пользователей

--- README.md
+++ README.md
@@ -14,3 +14,6 @@
 
 `npm run start` — запускает сервер   
 `npm run dev` — запускает сервер с hot-reload
+
+##  Cсылка на репозиторий проекта
+https://github.com/iammacheta/express-mesto-gha
---
+++ controllers/cards.js
@@ -0,0 +1,16 @@
+const Card = require('../models/card');
+
+module.exports.getAllCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.send({ data: cards }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.createCard = (req, res) => {
+  const { name, link } = req.body;
+  const owner = req.user._id;
+
+  Card.create({ name, link, owner })
+    .then((card) => res.send({ data: card }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
