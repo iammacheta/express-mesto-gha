524412dc9c154cc9656409802fbba1e8c68e4d0d
Maxim Ichetovkin <ichetovkin.maxim@gmail.com>
2022-11-11 11:13:14 +0800

    feat: реализовать дизлайк

--- controllers/cards.js
+++ controllers/cards.js
@@ -29,4 +29,14 @@ module.exports.likeCard = (req, res) => {
   )
     .then((card) => res.send(card))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
-}
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  )
+    .then((card) => res.send(card))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,11 @@
 const cards = require('express').Router();
 
-const { getAllCards, createCard, deleteCard, likeCard } = require('../controllers/cards')
+const { getAllCards, createCard, deleteCard, likeCard, dislikeCard } = require('../controllers/cards')
 
 cards.get('/', getAllCards);
 cards.post('/', createCard);
 cards.delete('/:cardId', deleteCard);
 cards.put('/:cardId/likes', likeCard);
+cards.delete('/:cardId/likes', dislikeCard);
 
 module.exports = cards;
