83754c2e84509f9ab3e2710d35788d5c46ff5a89
Maxim Ichetovkin <ichetovkin.maxim@gmail.com>
2023-01-13 11:01:19 +0800

    fix: вынести ссылку на БД в env переменные

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ const NotFoundError = require('./errors/NotFoundError');
 const { URL_REG_EXP } = require('./utils/constants');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_ADDRESS } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect(DB_ADDRESS, {
   useNewUrlParser: true,
 });
 
